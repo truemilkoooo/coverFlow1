fda21b9b62375705e74e36b6255a84f71bf3f214
truemilkoooo <truemilkoooo@gmail.com>
2022-04-06 18:35:30 +0300

    Initial Commit

---
+++ coverFlow1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coverFlow1
+//
+//  Created by Вова Пупкин on 06.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ coverFlow1UITests/coverFlow1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  coverFlow1UITestsLaunchTests.swift
+//  coverFlow1UITests
+//
+//  Created by Вова Пупкин on 06.04.2022.
+//
+
+import XCTest
+
+class coverFlow1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
